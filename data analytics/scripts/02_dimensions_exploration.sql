566d6839a4d909844677d0ea21a74cc83ff31495
dharmeshrohit <dharmeshrohit.online@gmail.com>
2025-06-03 13:12:59 +0530

    added 02 dimensions exploration

---
+++ data analytics/scripts/02_dimensions_exploration.sql
@@ -0,0 +1,28 @@
+
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+
+-- retrieve a list of unique countries from which customers originate
+select distinct 
+    country 
+from gold.dim_customers
+order by country;
+
+-- retrieve a list of unique categories, subcategories, and products
+select distinct 
+    category, 
+    subcategory, 
+    product_name 
+from gold.dim_products
+order by category, subcategory, product_name;
